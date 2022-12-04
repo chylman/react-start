1eed78dc918f7b2aa9844219e83297a67078e59d
Ilja Afanasev <chylmanprosto@gmail.com>
2022-12-04 12:15:43 +0300

    Добавляет список

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React, {useState} from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import './styles/App.css';
-import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 
 function App() {
-  const [value, setValue] = useState('ТЕКСТ В ИНПУТЕ')
+    const [posts, setProps] = useState([
+        {id: 1, title: 'Javascript', body: 'Description'},
+        {id: 2, title: 'Javascript 2', body: 'Description'},
+        {id: 3, title: 'Javascript 3', body: 'Description'},
+        {id: 4, title: 'Javascript 4', body: 'Description'},
+    ])
   return (
     <div className="App">
-      <PostItem/>
-      <PostItem/>
-      <PostItem/>
-      <PostItem/>
+        <PostList posts={posts}/>
     </div>
   );
 }
